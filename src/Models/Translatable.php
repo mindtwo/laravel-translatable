244c914712de74ae8611646c18c5fbbce55123ee
Jonas Emde <emde@mindtwo.de>
2024-01-24 17:42:31 +0100

    Add model properties

--- src/Models/Translatable.php
+++ src/Models/Translatable.php
@@ -2,10 +2,22 @@
 
 namespace mindtwo\LaravelTranslatable\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use mindtwo\LaravelAutoCreateUuid\AutoCreateUuid;
 
+/**
+ * @property ?int $id
+ * @property ?string $uuid
+ * @property ?string $translatable_type
+ * @property ?int $translatable_id
+ * @property ?string $key
+ * @property ?string $locale
+ * @property ?string $text
+ * @property ?Carbon $created_at
+ * @property ?Carbon $updated_at
+ */
 class Translatable extends Model
 {
     use AutoCreateUuid;
