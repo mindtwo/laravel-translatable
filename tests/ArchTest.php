ecbe184d111cccb9060fa0d45085c4cf6ad97cce
Andreas Schneider <schneider@mindtwo.de>
2024-06-14 15:41:16 +0200

    fix arch test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,16 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+arch('it will not use debugging functions dd')
+    ->expect('dd')
+    ->not->toBeUsed()
+    ->ignoring('mindtwo\LaravelTranslatable\Nova\Fields');
+
+arch('it will not use debugging functions dump')
+    ->expect('dump')
+    ->not->toBeUsed()
+    ->ignoring('mindtwo\LaravelTranslatable\Nova\Fields');
+
+arch('it will not use debugging functions ray')
+    ->expect('ray')
+    ->not->toBeUsed()
+    ->ignoring('mindtwo\LaravelTranslatable\Nova\Fields');
