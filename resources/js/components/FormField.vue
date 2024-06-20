9104edacdf76849f563e2d713176fd302d8aea8b
Andreas Schneider <schneider@mindtwo.de>
2024-06-20 13:46:57 +0200

    add support for certain field types

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -3,14 +3,13 @@
         <DefaultField
             v-for="locale in locales"
             :key="locale"
-            :field="field"
-            :errors="errors"
+            :field="currentField"
             :show-help-text="showHelpText"
             :full-width-content="fullWidthContent"
         >
 
             <FormLabel
-                :label-for="labelFor || field.uniqueKey"
+                :label-for="getLabelFor(locale)"
                 class="space-x-1"
                 :class="{ 'mb-2': shouldShowHelpText }"
             >
@@ -23,24 +22,52 @@
             </FormLabel>
 
             <template #field>
+                <textarea
+                    v-if="inputType === 'textarea'"
+                    v-bind="extraAttributes"
+                    class="block w-full form-control form-input form-control-bordered py-3 h-auto"
+                    :id="getLabelFor(locale)"
+                    :dusk="field.attribute"
+                    :value="value[locale]"
+                    @input="handleChange($event, locale)"
+                    :maxlength="field.enforceMaxlength ? field.maxlength : -1"
+                    :placeholder="placeholder"
+                />
+                <MarkdownEditor
+                    v-else-if="inputType === 'markdown'"
+                    :ref="`mdEditor-${locale}`"
+                    v-show="currentlyIsVisible"
+                    :class="{ 'form-control-bordered-error': hasError }"
+                    :id="getLabelFor(locale)"
+                    :previewer="previewer"
+                    :readonly="currentlyIsReadonly"
+                    @initialize="initialize(locale)"
+                    @change="handleChange($event, locale)"
+                />
                 <input
-                    :id="field.attribute"
-                    type="text"
+                    v-else
+                    :id="getLabelFor(locale)"
+                    :type="inputType"
                     class="w-full form-control form-input form-control-bordered"
                     :class="errorClasses"
                     :placeholder="field.name"
                     v-model="value[locale]"
                 />
+
+                <HelpText class="help-text-error" v-if="localeErrors[locale]">
+                    {{ localeErrors[locale][0] }}
+                </HelpText>
             </template>
         </DefaultField>
     </div>
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
+import isNil from 'lodash/isNil';
+import { DependentFormField, HandlesValidationErrors } from 'laravel-nova';
 
 export default {
-    mixins: [FormField, HandlesValidationErrors],
+    mixins: [DependentFormField, HandlesValidationErrors],
 
     props: ['resourceName', 'resourceId', 'field'],
 
@@ -61,9 +88,48 @@ export default {
         fieldName() {
             return this.field.name;
         },
+        inputType() {
+            return this.field.inputType;
+        },
+
+        localeErrors() {
+            const { errors } = this.errors;
+
+            if (!errors || !errors[this.field.attribute]) {
+                return {};
+            }
+
+            const fieldErrors = errors[this.field.attribute];
+
+            return fieldErrors.reduce((acc, error) => {
+                const obj = JSON.parse(error);
+
+                const entries = Object.entries(obj);
+
+                for (const [locale, message] of entries) {
+                    if (!acc[locale]) {
+                        acc[locale] = [];
+                    }
+
+                    acc[locale].push(message);
+                }
+
+                return acc;
+            }, {});
+        },
+
+        // TODO preview?
+        // previewer() {
+        //     if (!this.isActionRequest) {
+        //         return this.fetchPreviewContent
+        //     }
+        // },
     },
 
     methods: {
+        getLabelFor(locale) {
+            return `${this.field.uniqueKey}-${locale}`;
+        },
         /*
         * Set the initial, internal value for the field.
         */
@@ -71,18 +137,109 @@ export default {
             this.value = {};
 
             for (const locale of this.locales) {
-                this.value[locale] = this.field.value[locale] || '';
+                this.value[locale] = this.field.value?.[locale] || '';
             }
+        },
+
+        getErrors(locale) {
+            return this.localeErrors[locale] || [];
         },
 
         /**
          * Fill the given FormData object with the field's internal value.
          */
         fill(formData) {
-            for (const locale of this.locales) {
-                formData.append(`${this.field.attribute}[${locale}]`, this.value[locale] || '');
+            this.fillIfVisible(formData, this.field.attribute, JSON.stringify(this.value));
+        },
+
+        /**
+         * Update the field's internal value
+         */
+        handleChangeMarkdown(value, locale) {
+            this.value[locale] = value;
+
+            if (this.field) {
+                this.emitFieldValueChange(this.fieldAttribute, this.value)
             }
         },
+
+        /**
+         * Update the field's internal value
+         */
+        handleChangeTextarea(event, locale) {
+            this.value[locale] = event.target.value;
+
+            if (this.field) {
+                this.emitFieldValueChange(this.fieldAttribute, this.value)
+                this.$emit('field-changed')
+            }
+        },
+
+        /**
+         * Update the field's internal value
+         */
+        handleChange(event, locale) {
+            if (this.inputType === 'markdown') {
+                // event is only the value
+                this.handleChangeMarkdown(event, locale);
+                return;
+            }
+
+            if (this.inputType === 'textarea') {
+                this.handleChangeTextarea(event, locale);
+                return;
+            }
+        },
+
+        listenToValueChanges(value) {
+            if (this.currentlyIsVisible) {
+                this.$refs.theMarkdownEditor.setValue(value)
+            }
+
+            this.handleChange(value)
+        },
+
+        /**
+         * Initialize the Markdown Editor
+         */
+        initialize(locale) {
+            const mdEditor = this.$refs[`mdEditor-${locale}`];
+
+            if (!mdEditor?.length) {
+                return;
+            }
+
+            mdEditor[0].setValue(this.value[locale] ?? this.currentField.value[locale]);
+
+            Nova.$on(`${this.fieldAttributeValueEventName}:${locale}`, this.listenToValueChanges);
+        },
+
+        async fetchPreviewContent(value) {
+            Nova.$progress.start()
+
+            const {
+                data: { preview },
+            } = await Nova.request().post(
+                `/nova-api/${this.resourceName}/field/${this.fieldAttribute}/preview`,
+                { value },
+                {
+                    params: {
+                        editing: true,
+                        editMode: isNil(this.resourceId) ? 'create' : 'update',
+                    },
+                }
+            )
+
+            Nova.$progress.done()
+
+            return preview
+        },
+    },
+
+    beforeUnmount() {
+        this.locales.forEach(locale => {
+            Nova.$off(`${this.fieldAttributeValueEventName}:${locale}`, this.listenToValueChanges);
+        });
     },
 }
 </script>
--- src/Nova/Fields/TranslatableField.php
+++ src/Nova/Fields/TranslatableField.php
@@ -2,24 +2,111 @@
 
 namespace mindtwo\LaravelTranslatable\Nova\Fields;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\SupportsDependentFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Resource;
+use mindtwo\LaravelTranslatable\Contracts\IsTranslatable;
 
 class TranslatableField extends Field
 {
+    use SupportsDependentFields;
+
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
     public $component = 'translatable-field';
+
+    public $inputType = 'text';
+
+    public ?Resource $repeateableResource = null;
 
     public function __construct($name, ?string $key = null)
     {
         parent::__construct($name, $key, function ($value, $resource, $attribute) {
+
+            // If the value is a collection, convert it to an array
+            if ($value instanceof Collection) {
+                $value = $value->toArray();
+            }
+
+            if (empty($value)) {
+                return null;
+            }
+
             // Create a value map locale => translation
             $value = collect($value)->mapWithKeys(function ($item) {
                 return [$item['locale'] => $item['text']];
-            });
+            })->toArray();
 
             return $value;
+
         });
 
         $this->key($key);
+    }
+
+    /**
+     * Set the validation rules for the field.
+     *
+     * @param  (callable(\Laravel\Nova\Http\Requests\NovaRequest):(array|\Stringable|string|callable))|array|\Stringable|string  ...$rules
+     * @return @return array<array-key, array<int, mixed>>
+     */
+    public function getRules(NovaRequest $request)
+    {
+        return $this->getLocaleRules($request, true);
+    }
+
+    protected function getLocaleRules(NovaRequest $request, bool $withLocales = false): array
+    {
+        $rules = is_callable($this->rules) ? call_user_func($this->rules, $request) : $this->rules;
+
+        if (! $withLocales) {
+            return [
+                $this->attribute => $rules,
+            ];
+        }
+
+        // Validate the field for each locale
+        return [
+            $this->attribute => function (string $attribute, mixed $value, \Closure $fail) use ($rules) {
+                $value = json_decode($value, true);
+                if (empty($value) || ! is_array($value)) {
+                    $fail("The {$attribute} is invalid.");
+
+                    return;
+                }
+
+                // Create a rule map for each locale
+                $localeRuleMap = array_reduce($this->meta['locales'], function ($carry, $locale) use ($rules) {
+                    $carry[$locale] = $rules;
+
+                    return $carry;
+                }, []);
+
+                // Use the rule map to validate the value
+                $validator = Validator::make($value, $localeRuleMap);
+
+                if ($validator->fails()) {
+                    $fail(
+                        collect($validator->messages())
+                            ->map(function ($message, $locale) use ($attribute) {
+                                $localeKey = str_replace('_', ' ', Str::snake($locale));
+
+                                $attributeName = Arr::get(app('translator')->get('validation.attributes'), $attribute);
+
+                                return Str::replace($localeKey, $attributeName, $message[0]);
+                            })
+                    );
+                }
+            },
+        ];
     }
 
     /**
@@ -30,8 +117,51 @@ class TranslatableField extends Field
      */
     public function fillModelWithData(mixed $model, mixed $value, string $attribute)
     {
+        // skip if the model is a Fluent instance
+        if ($model instanceof \Laravel\Nova\Support\Fluent) {
+            $attributes = $model->getAttributes();
+
+            $model = $this->model::find($attributes['id']);
+
+            if (! $model) {
+                return;
+            }
+
+            $this->upsertModelTranslation($model, $value, $attribute);
+
+            return;
+        }
+
+        // If the model does not implement the IsTranslatable interface, throw an exception
         if (! $model instanceof \mindtwo\LaravelTranslatable\Contracts\IsTranslatable) {
             throw new \InvalidArgumentException('The model must implement the IsTranslatable interface.');
+        }
+
+        $this->upsertModelTranslation($model, $value, $attribute);
+    }
+
+    /**
+     * Create or update translations for the given model.
+     */
+    protected function upsertModelTranslation(IsTranslatable $model, mixed $value, string $attribute): void
+    {
+        // If the value is a string, try to decode it
+        if (is_string($value)) {
+            $value = json_decode($value, true);
+        }
+
+        // Skip if the value is not an array
+        if (! is_array($value)) {
+            return;
+        }
+
+        dump($attribute);
+        if (! $model->exists) {
+            $model::created(function ($model) use ($value, $attribute) {
+                $this->upsertModelTranslation($model, $value, $attribute);
+            });
+
+            return;
         }
 
         // Create or update translations from the given value
@@ -39,7 +169,7 @@ class TranslatableField extends Field
             if (empty($translation)) {
                 $model->translations()->where([
                     'locale' => $locale,
-                    'key' => $this->meta['key'],
+                    'key' => $attribute,
                 ])->delete();
 
                 continue;
@@ -48,11 +178,39 @@ class TranslatableField extends Field
             $model->translations()->updateOrCreate(
                 [
                     'locale' => $locale,
-                    'key' => $this->meta['key'],
+                    'key' => $attribute,
                 ],
                 ['text' => $translation]
             );
         }
+    }
+
+    /**
+     * Determine if the field is required.
+     *
+     * @return bool
+     */
+    public function isRequired(NovaRequest $request)
+    {
+        // Get the base rules
+        $rules = is_callable($this->rules) ? call_user_func($this->rules, $request) : $this->rules;
+
+        if (! empty($this->attribute)) {
+            if ($request->isResourceIndexRequest() || $request->isLensRequest() || $request->isActionRequest()) {
+                return in_array('required', $this->getLocaleRules($request)[$this->attribute]);
+            }
+
+            if ($request->isCreateOrAttachRequest()) {
+                return in_array('required', $this->getLocaleRules($request)[$this->attribute]);
+            }
+
+            if ($request->isUpdateOrUpdateAttachedRequest()) {
+                return in_array('required', $this->getLocaleRules($request)[$this->attribute]);
+            }
+        }
+
+        // If the field is not required, return false
+        return in_array('required', $rules);
     }
 
     /**
@@ -83,6 +241,18 @@ class TranslatableField extends Field
     }
 
     /**
+     * Set the model for the field.
+     *
+     * @return $this
+     */
+    public function model(string $model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
      * Set the key for the field.
      *
      * @param  string  $key
@@ -91,5 +261,66 @@ class TranslatableField extends Field
     public function key($key)
     {
         return $this->withMeta(['key' => $key]);
+    }
+
+    public function markdown()
+    {
+        return $this->inputType('markdown');
+    }
+
+    public function textarea()
+    {
+        return $this->inputType('textarea');
+    }
+
+    /**
+     * Set the input component for the field.
+     *
+     * @param  string  $component
+     * @return $this
+     */
+    public function inputType($component)
+    {
+        $this->inputType = $component;
+
+        return $this;
+    }
+
+    /**
+     * Prepare the field for JSON serialization.
+     *
+     * @return array<string, mixed>
+     */
+    public function jsonSerialize(): array
+    {
+        return array_merge(parent::jsonSerialize(), [
+            'inputType' => $this->inputType,
+        ]);
+    }
+
+    /**
+     * Resolve the element's value.
+     *
+     * @param  mixed  $resource
+     * @param  string|null  $attribute
+     * @return void
+     */
+    public function resolve($resource, $attribute = null)
+    {
+        $attribute = $attribute ?? $this->attribute;
+
+        return with(app(NovaRequest::class), function ($request) use ($resource, $attribute) {
+            if (! $request->isFormRequest() || ! in_array($request->method(), ['PUT', 'PATCH'])) {
+                return parent::resolve($resource, $attribute);
+            }
+
+            $value = $request->input($this->attribute);
+
+            if (! is_string($value)) {
+                return $value;
+            }
+
+            return json_decode($value, true);
+        });
     }
 }
